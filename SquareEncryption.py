3a3c5b00bff61a21636cd8a8369a86ce22b0ddda
Chris Cantey <cantey0701@gmail.com>
2015-07-30 10:01:45 -0500

    Update SquareEncryption.py

--- SquareEncryption.py
+++ SquareEncryption.py
@@ -1,18 +1,18 @@
 # A fairly simple way to encrypt a mesage is to strip all spaces and special characters from the message,
 # and split the message into a 2-dimensional array, then transpose the array. example:
 
-message = "How is everyone doing today"
-square message = [H, o, w, i, s]
-                 [e, v, e, r, y]
-                 [o, n, e, d, o]
-                 [i, n, g, t, o]
-                 [d, a, y, 0, 0]
+# message = "How is everyone doing today"
+# square message = [H, o, w, i, s]
+#                  [e, v, e, r, y]
+#                  [o, n, e, d, o]
+#                  [i, n, g, t, o]
+#                  [d, a, y, 0, 0]
                  
-encrypted message = [H, e, o, i, d]
-                    [o, v, n, n, a]
-                    [w, e, e, g, y]
-                    [i, r, d, t, 0]
-                    [s, y, o, o, 0]
+# encrypted message = [H, e, o, i, d]
+#                     [o, v, n, n, a]
+#                     [w, e, e, g, y]
+#                     [i, r, d, t, 0]
+#                     [s, y, o, o, 0]
 
 # Create a program that takes a user input, screens it for special characters, strips the spaces then encodes the message
 # using this square methodology
